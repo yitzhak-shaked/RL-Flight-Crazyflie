911e3adfc4e23e3ff6fb9a0f8dbd43c488f4955b
Shaked Yitzhak <shaked8900@gmail.com>
2025-10-27 00:08:38 +0200

    fix: Increase position termination threshold from 0.6m to 1.5m
    CRITICAL BUG FIX: The drone was physically prevented from reaching the 1.0m
    target because episodes terminated when any axis exceeded 0.6m from origin.
    
    Changes:
    - Increased position_threshold from 0.6 to 1.5 in both classic and fast_learning
    - This allows the drone to reach 1.0m target with 0.5m safety margin
    - Explains why all training attempts stopped at ~0.5m regardless of other fixes
    
    Impact:
    - Agents can now physically reach the target position at (0, 1.0, 0)
    - Training should progress beyond the previous 0.5m barrier

--- include/learning_to_fly/simulator/parameters/termination/default.h
+++ include/learning_to_fly/simulator/parameters/termination/default.h
@@ -7,14 +7,14 @@ namespace rl_tools::rl::environments::multirotor::parameters::termination{
     template<typename T, typename TI, TI ACTION_DIM, typename REWARD_FUNCTION>
     constexpr typename rl_tools::rl::environments::multirotor::ParametersBase<T, TI, 4, REWARD_FUNCTION>::MDP::Termination classic = {
             true,           // enable
-            0.6,            // position
+            1.5,            // position - increased from 0.6 to allow reaching 1m target
             10,         // linear velocity
             10 // angular velocity
     };
     template<typename T, typename TI, TI ACTION_DIM, typename REWARD_FUNCTION>
     constexpr typename rl_tools::rl::environments::multirotor::ParametersBase<T, TI, 4, REWARD_FUNCTION>::MDP::Termination fast_learning = {
         true,           // enable
-        0.6,            // position
+        1.5,            // position - increased from 0.6 to allow reaching 1m target
         1000,         // linear velocity
         1000 // angular velocity
     };
